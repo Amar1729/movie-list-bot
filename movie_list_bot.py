61b204a6da0357974ca8acf0cc52e2dd5c017118
Amar1729 <amar.paul16@gmail.com>
2020-10-02 00:16:08 -0400

    remove telepot dependency

--- movie_list_bot.py
+++ movie_list_bot.py
@@ -9,8 +9,6 @@ import time
 from pathlib import Path
 from typing import Tuple
 
-import telepot
-
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 
 # local settings.py with KEY (received from BotFather)
@@ -42,9 +40,6 @@ HELP_STRING = (
     INTRO
     + "\n".join("{}: {}".format(c[0], c[1]) for c in COMMANDS.items())
 )
-
-BOT = telepot.Bot(KEY)
-
 
 
 class Movies:
@@ -259,118 +254,6 @@ def finished_list(update, context):
         update.message.reply_text(
             "This chat hasn't finished any movies! Add them with /watched."
         )
-
-
-def handle(msg):
-    if not ("text" in msg and any(msg["text"].startswith(k) for k in COMMANDS)):
-        return
-
-    chat_id = msg["chat"]["id"]
-    command = msg["text"]
-
-    print("got command: {}".format(command))
-
-    # Add a movie to the watchlist (creates the group if it didn't exist yet)
-    if command.startswith("/add"):
-        try:
-            first_space = command.index(" ")
-            movie_list = command[first_space + 1 :].strip()
-        except ValueError:
-            movie_list = None
-
-        if not movie_list:
-            BOT.sendMessage(chat_id, "Make sure to include a movie title with /add")
-        else:
-            for movie in movie_list.split("\n"):
-                if MOVIES.add_movie(chat_id, movie.strip()):
-                    BOT.sendMessage(chat_id, "'{}' added to list".format(movie.strip()))
-                else:
-                    BOT.sendMessage(
-                        chat_id, "'{}' already on list".format(movie.strip())
-                    )
-
-    # Get the movie watchlist
-    elif command.startswith("/list"):
-        ret = MOVIES.list_movies(chat_id)
-        if ret:
-            BOT.sendMessage(chat_id, "Your list:\n{}".format(ret))
-        else:
-            BOT.sendMessage(chat_id, "No movie list yet! Add movies with /add.")
-
-    # Get the list of finished movies
-    elif command.startswith("/finished"):
-        ret = MOVIES.finished_movies(chat_id)
-        if ret:
-            BOT.sendMessage(chat_id, "You've watched: \n{}".format(ret))
-        else:
-            BOT.sendMessage(
-                chat_id, "This chat hasn't finished any movies! Add them with /watched."
-            )
-
-    elif command.startswith("/random"):
-        try:
-            count = int(command.split(" ")[1])
-        except ValueError:
-            BOT.sendMessage(chat_id, "Argument `n` must be an integer")
-            return
-        except IndexError:
-            count = 1
-
-        movie_list = MOVIES.get_random(chat_id, count)
-        if movie_list:
-            BOT.sendMessage(chat_id, movie_list)
-        else:
-            BOT.sendMessage(
-                chat_id,
-                "Not enough movies ({}) in movie list! Check with /list.".format(count),
-            )
-
-    # Mark a movie as watched
-    elif command.startswith("/watched"):
-        try:
-            first_space = command.index(" ")
-            movie_list = command[first_space + 1 :].strip()
-        except ValueError:
-            movie_list = None
-
-        if not movie_list:
-            BOT.sendMessage(chat_id, "Make sure to include a movie title with /watched")
-        else:
-            for movie in movie_list.split("\n"):
-                MOVIES.watched_a_movie(chat_id, movie)
-                BOT.sendMessage(
-                    chat_id, "Added '{}' to your finished list!".format(movie)
-                )
-
-    elif command.startswith("/modify"):
-        first_space = command.index(" ")
-        movienum = command[first_space + 1 :]
-
-        pass
-
-    elif command.startswith("/remove"):
-        try:
-            movie_num = int(command.split(" ")[1])
-            movie_name = MOVIES.remove_movie(chat_id, movie_num)
-            if movie_name:
-                BOT.sendMessage(chat_id, "Removed '{}'".format(movie_name))
-            else:
-                BOT.sendMessage(
-                    chat_id,
-                    "Not enough items in list (invalid number?): {}".format(movie_num),
-                )
-        except (IndexError, ValueError):
-            BOT.sendMessage(chat_id, "/remove requires a list item's number.")
-
-    # help string
-    elif command.startswith("/help"):
-        intro = "Movie List BOT! A BOT for keeping track of movies to watch with your friends."
-        help_string = (
-            intro
-            + "\n"
-            + "\n".join("{}: {}".format(c[0], c[1]) for c in COMMANDS.items())
-        )
-        BOT.sendMessage(chat_id, help_string)
 
 
 def main():
