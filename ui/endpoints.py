dda928a30685cc3509fc0f628722ed2faeee32e3
Amar1729 <amar.paul16@gmail.com>
2020-11-08 04:35:23 -0500

    function for displaying movie info

--- ui/endpoints.py
+++ ui/endpoints.py
@@ -37,6 +37,27 @@ def _search_imdb(title: str, limit: int = 5):
             break
 
 
+def create_message(movie_id: str):
+    """ Displays a movie, given an IMDB movie ID """
+    movie = IA.get_movie(movie_id, info=["main"])
+
+    return "\n".join([
+        Emoji.MOVIE + movie["title"],
+        "",
+        f"Year: {movie['year']}",
+        "Genres:" + ", ".join(movie['genres']),
+        "",
+        "Plot",
+        movie["plot outline"],
+        "",
+        f"Rating: {movie['rating']} / 10.0",
+        "",
+        # TODO - can this be formatted as markdown?
+        # f"[thumbnail]\\({movie['cover url']}\\)",
+        movie["cover url"],
+    ])
+
+
 def inline_search(update, context):
     """ Search trakt for a movie """
     # chat_id = update.message.chat_id
